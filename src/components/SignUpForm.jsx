95713d6ff041d8d18610d0f4c78badf064a62916
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-10-13 14:02:39 -0600

    seventh  commit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -25,17 +25,22 @@ return (
         <label>
             Username:{""}
             <input 
+            type="text"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            />
+            onChange={(e) => {
+                console.log(e.target.value)
+                setUsername(e.target.value)
+            }}/>
         </label>
         <label>
             Password:{""}
             <input
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
+            onChange={(e) => {
+                console.log(e.target.value)
+                setPassword(e.target.value)
+            }}/>
             
             </label>
            <button>Submit</button> 
@@ -43,3 +48,4 @@ return (
     </>
 );
 }
+
